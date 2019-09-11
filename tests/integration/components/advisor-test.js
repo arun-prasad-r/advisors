bc2fe3f8a98a5f3f895ec9b63b3cc02a87781bf0
Arun Prasad <arun@arunprasad.info>
2019-09-11 21:02:39 +0530

    (test) Add integration test for advisor component

--- tests/integration/components/advisor-test.js
+++ tests/integration/components/advisor-test.js
@@ -3,24 +3,86 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | advisor', function(hooks) {
+const testAdvisor = {
+  name: 'John Doe',
+  bio: 'blah blah',
+  callAvailability: false,
+  chatAvailability: false
+};
+
+module('Integration | Component | advisor', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{advisor}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#advisor}}
-        template block text
-      {{/advisor}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test('it should not show admin controls when adminMode is false', async function (assert) {
+    assert.expect(1);
+    //set advisor model 
+    this.set('advisor', testAdvisor);
+    //set adminModeEnabled to false
+    this.set('adminModeEnabled', false);
+    await render(hbs`<Advisor @advisor={{this.advisor}} @adminModeEnabled={{this.adminModeEnabled}}/>`);
+    assert.dom('.admin.column').doesNotExist();
   });
+
+  test('it should show admin controls when adminMode is true', async function (assert) {
+    assert.expect(1);
+    //set advisor model 
+    this.set('advisor', testAdvisor);
+    //set adminModeEnabled to false
+    this.set('adminModeEnabled', true);
+    await render(hbs`<Advisor @advisor={{this.advisor}} @adminModeEnabled={{this.adminModeEnabled}}/>`);
+    assert.dom('.admin.column').exists();
+  });
+
+  test('Call Now button should be disabled when callAvailability is false', async function (assert) {
+    assert.expect(1);
+    //set advisor model 
+    let advisor = Object.assign({}, testAdvisor);
+    advisor.callAvailability = false;
+
+    this.set('advisor', advisor);
+    //set adminModeEnabled to false
+    this.set('adminModeEnabled', true);
+    await render(hbs`<Advisor @advisor={{this.advisor}} @adminModeEnabled={{this.adminModeEnabled}}/>`);
+    assert.dom('.call_now.button').hasClass('grey');
+  });
+
+  test('Call Now button should be teal when callAvailability is true', async function (assert) {
+    assert.expect(1);
+    //set advisor model 
+    let advisor = Object.assign({}, testAdvisor);
+    advisor.callAvailability = true;
+
+    this.set('advisor', advisor);
+    //set adminModeEnabled to false
+    this.set('adminModeEnabled', true);
+    await render(hbs`<Advisor @advisor={{this.advisor}} @adminModeEnabled={{this.adminModeEnabled}}/>`);
+    assert.dom('.call_now.button').hasClass('teal');
+  });
+
+  test('Chat Now button should be teal when chatAvailability is true', async function (assert) {
+    assert.expect(1);
+    //set advisor model 
+    let advisor = Object.assign({}, testAdvisor);
+    advisor.chatAvailability = true;
+
+    this.set('advisor', advisor);
+    //set adminModeEnabled to false
+    this.set('adminModeEnabled', true);
+    await render(hbs`<Advisor @advisor={{this.advisor}} @adminModeEnabled={{this.adminModeEnabled}}/>`);
+    assert.dom('.chat_now.button').hasClass('teal');
+  });
+
+  test('Chat Now button should be grey when chatAvailability is false', async function (assert) {
+    assert.expect(1);
+    //set advisor model 
+    let advisor = Object.assign({}, testAdvisor);
+    advisor.chatAvailability = false;
+
+    this.set('advisor', advisor);
+    //set adminModeEnabled to false
+    this.set('adminModeEnabled', false);
+    await render(hbs`<Advisor @advisor={{this.advisor}} @adminModeEnabled={{this.adminModeEnabled}}/>`);
+    assert.dom('.chat_now.button').hasClass('grey');
+  });
+
 });
